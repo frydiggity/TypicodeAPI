bc440399f2dee1b67aef555dae3bc3b2e0ede5a5
Matt Freiburg <matt@thefreiburgs.com>
2025-03-01 09:17:10 -0600

    Add Empty App project

---
+++ TypicodeAPI/TypicodeAPI/TypicodeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypicodeAPIApp.swift
+//  TypicodeAPI
+//
+//  Created by Matt Freiburg on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct TypicodeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TypicodeAPI/TypicodeAPITests/TypicodeAPITests.swift
@@ -0,0 +1,17 @@
+//
+//  TypicodeAPITests.swift
+//  TypicodeAPITests
+//
+//  Created by Matt Freiburg on 3/1/25.
+//
+
+import Testing
+@testable import TypicodeAPI
+
+struct TypicodeAPITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
